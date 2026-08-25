8f5c99532418a9438ebbc644cb02baeb318261bc
ManuBarath Kanakarajan <manubarath33@gmail.com>
2026-08-26 00:46:23 +0530

    Python first push

---
+++ testing.py
@@ -0,0 +1,6 @@
+import pandas as pd
+
+print("---------------------------------------")
+print("SUCCESS: Virtual Environment is Active!")
+print("Pandas Version:", pd.__version__)
+print("---------------------------------------")
